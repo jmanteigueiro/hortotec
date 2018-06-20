fd45b711e3142d00e592d41839e46cfcfd20c13b
Jose Manteigueiro <jmant@protonmail.com>
2018-06-20 16:46:31 +0000

    Updates

--- greenhouse.py
+++ greenhouse.py
@@ -1,28 +1,59 @@
+from __future__ import division
 from soil_return import soilHumidity
 from water_return import waterPump
+from luminosity_return import luminosity
 import RPi.GPIO as GPIO
 import time
 import csv
 
-PIN_HUMIDADE_TERRA = 37
+PIN_SENSOR_LUMINOSIDADE = 7
+PIN_SENSOR_HUMIDADE_TERRA = 37
 PIN_BOMBA_AGUA = 40
 TEMPO_BOMBA_AGUA = 2 #segundos
 
+def percentagem(valor_maximo, valor_atual):
+    percent = (valor_atual * 100 / valor_maximo)
+    percent2 = ('%.1f' % percent).rstrip('0').rstrip('.')
+    return percent2
+
+def percentagem_luminosidade(valor_maximo, valor_minimo, valor_atual):
+    intervalo = valor_minimo - valor_maximo
+    percent =  100- ((valor_atual / intervalo) * 100)
+    #return '{0:.1g}'.format(percent)
+    percent2 = ('%.1f' % percent).rstrip('0').rstrip('.')
+    return percent2
+
+
 try:
-    soilHumidity(PIN_HUMIDADE_TERRA) #Para descartar o primeiro erro de leitura
+    soilHumidity(PIN_SENSOR_HUMIDADE_TERRA) #Para descartar o primeiro erro de leitura
+    time.sleep(1)
+    waterPump(PIN_BOMBA_AGUA, TEMPO_BOMBA_AGUA)
     while True:
         time.sleep(1)
-        csv = open('humidade.csv', 'a+')
+        #csv = open('humidade.csv', 'a+')
+        valores_atuais = open('valores_atuais.txt', "w")
         localtime = time.asctime(time.localtime(time.time()))
-        humidity = soilHumidity(PIN_HUMIDADE_TERRA)
-        print("Humidade: " + str(humidity))
-        csv.write(str(humidity) + "," + str(localtime))
-        if(humidity < 1000):
-            waterPump(PIN_BOMBA_AGUA, TEMPO_BOMBA_AGUA)
-            csv.write(", Ativar Rega!")
-        csv.write("\n")
-        csv.close()
-        time.sleep(898) #15min de espera
+        humidity = soilHumidity(PIN_SENSOR_HUMIDADE_TERRA)
+        time.sleep(1)
+        lumino = luminosity(PIN_SENSOR_LUMINOSIDADE)
+        time.sleep(1)
+        hum_perc = percentagem(300000, humidity)
+        lum_perc = percentagem_luminosidade(500, 50000, lumino)
+        print("HUM: " + str(humidity) + " Percent: " + str(hum_perc))
+        print("LUM: " + str(lumino) + " Percent: " + str(lum_perc))
+        #print("Humidade: " + str(humidity))
+
+        #escrita dos dados
+        valores_atuais.write(str(hum_perc)+","+str(lum_perc)+","+str("25")+",")
+
+        valores_atuais.write(str(localtime))
+        #csv.write(str(humidity) + "," + str(localtime))
+        #if(humidity < 1000):
+        #    waterPump(PIN_BOMBA_AGUA, TEMPO_BOMBA_AGUA)
+        #    csv.write(", Ativar Rega!")
+        #csv.write("\n")
+        #csv.close()
+        time.sleep(10) #15min de espera
 except KeyboardInterrupt:
     print ("\nCtrl-C pressed.  Program exiting...")
 finally:
--- luminosity_return.py
+++ luminosity_return.py
@@ -5,7 +5,7 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 
-def soilHumidity(pin_to_circuit):
+def luminosity(pin_to_circuit):
     global count
     try:
         #time.sleep(0.1)
@@ -19,14 +19,16 @@ def soilHumidity(pin_to_circuit):
         # Change the pin back to input
         GPIO.setup(pin_to_circuit, GPIO.IN)
 
-        # Count until the pin goes high
+        # Count until the pin goes highz<
         while (GPIO.input(pin_to_circuit) == GPIO.LOW):
             count += 1
-            if(count > 300000):
+            if(count > 50000): # min
                 break
 
     except KeyboardInterrupt:
         return 0
     finally:
+        if(count < 500): # max
+            count = 500
         return count
 
